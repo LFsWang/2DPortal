64754a8d1c138f9b29dbfa7de42df1eaf1af9a4d
LFsWang <tnst92002@gmail.com>
2015-12-25 23:47:43 +0800

    FirstCommit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<Windows.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+
+#include"ui.h"
+
+#include"library\console_draw3.h"
+#include"library\font.h"
+#include"library\image.h"
+#include"library\kb_input2.h"
+#include"library\audio.h"
+
+int main()
+{
+	return 0;
+}
---
+++ ui.h
@@ -0,0 +1,13 @@
+#ifndef UI_H
+#define UI_H
+
+#include<Windows.h>
+
+bool SetFullScreen(){
+    keybd_event(VK_MENU,0x38,0,0);
+    keybd_event(VK_RETURN,0x1c,0,0);
+    keybd_event(VK_RETURN,0x1c,KEYEVENTF_KEYUP,0);
+    keybd_event(VK_MENU,0x38,KEYEVENTF_KEYUP,0);
+}
+
+#endif
